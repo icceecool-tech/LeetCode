1d64e0907c51f10d0c67ecda4738e1a2e086461c
SANDEEP KUMAR MANDAL <skmandal123sk@gmail.com>
2026-07-17 17:47:21 +0000

    Number hashing

--- 03-BasicHashing/01-numberHashing.cpp
+++ 03-BasicHashing/01-numberHashing.cpp
@@ -1,3 +1,24 @@
+//  Number hashing using arrays
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[] = { 2,3,4,5,16};
+    int hash[17]={0};
+    for(int i =0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        hash[arr[i]]++;
+    }
+    int q;
+    cin >> q;
+    while(q--){
+        int num;
+        cin >> num;
+        cout << hash[num] << endl;
+    }
+    return 0;
+}
+
+
+//  Number hashing using map/unordered map
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
